f1e04189bc0bccab1fdcf9c55eba6e4d0facc35a
Nir <nzagdansk@univ.haifa.ac.il>
2021-10-18 07:51:00 +0100

    logger datetime

--- lib/logger.py
+++ lib/logger.py
@@ -1,12 +1,24 @@
 import logging
 import sys
 import os
+import datetime
 class Logger:
     def __init__(self, test_mode: bool) -> None:
         self.log = logging.getLogger()
-        self.log.setLevel(logging.DEBUG)
+        self.log.setLevel(logging.NOTSET)
         console_handler = logging.StreamHandler(sys.stdout)
-        s = 'log.log'
+        # s = 'log.log'
+        #e.g. 2021-03-10 10:34:12.678331
+        current_datetime = str(datetime.datetime.now())
+
+        #e.g. 20210310
+        caurrent_date =  current_datetime.split(" ")[0].replace("-", "")
+
+        #e.g. 103412
+        current_time = current_datetime.split(" ")[1].split(".")[0].replace(":", "")
+
+        #e.g. 20210310_103412.log
+        s = log_name = caurrent_date + "_" + current_time + ".log"
         full_path = os.path.join('log',s)
         file_handler = logging.FileHandler(full_path)
         console_handler.setLevel(logging.WARNING)
